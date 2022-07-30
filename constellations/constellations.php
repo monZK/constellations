e29de62e6deb8dfb7ce4c7031759d303f5c19550
monZK <zk224804@outlook.com>
2022-07-29 19:41:13 -0500

    para constellations

--- constellations/constellations.php
+++ constellations/constellations.php
@@ -2,7 +2,28 @@
     require 'inc/fxs.php'
 ?>
 <div class="text-abousus">
-            <blockquote></blockquote>
+            <blockquote>
+            <blockquote>
+                            Haciendo que brilles, a través de nuestras estrellas...
+                        </blockquote>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum nihil quisquam corrupti! Aliquid iure ex deleniti blanditiis doloremque iste molestias reprehenderit aperiam doloribus incidunt ratione soluta debitis nemo, cupiditate vitae!</p>
+                        <p>
+                            <h4>Lic. Jonathan Bustamante</h4>
+
+                        </p>
+                        <p>
+                            <h4>Ingeniera Mónica Mendoza </h4>
+                        </p>
+                        <p>
+                            <h4>Ingeniero Oscar Sánchez</h4> 
+                            Lic. en Ingeniería en Sistemas Computacionales. Desde los 14 años me interesé en los temas de programación y reparación de computadoras para tener un ingreso extra para mis gastos diarios, comenzando a reparar equipos de cómputo a mis familiares y amigos lo cual me ayudó mucho a adentrarme más al tema de cómo funcionaba una computadora, empecé con los lenguajes de programación Visual Basic 6 y Visual FoxPro 6 que son lenguajes por procedimientos orientados a objetos, este par de herramientas me ayudaron mucho en parte de mi formación de preparatoria pues podía hacer programas que me ayudaran a hacer cálculos, gráficas y presentaciones en varias materias, pero al entrar a los estudios universitarios tuve la oportunidad de trabajar con lenguajes de bajo nivel como ensamblador(.asm) para microprocesadores y microcontroladores,  como lenguajes de alto nivel Python(.py) para minería de datos, estadística y probabilidad, como también me sumergí en la programación web con el framework Django, con el cual se construyó el sitio web para “Ingeniería Aplicada a Sistemas Energéticos” INASE, teniendo un proyecto más sobre puntos de venta y sistema de facturacion para restaurantes en el Estado de Guanajuato desde el 2017.
+                        </p>
+                        <p>
+                            <h4>Lic. Paulina Gutierrez </h4>
+
+                        </p>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni optio perferendis rem cum, debitis reiciendis inventore ipsa voluptates non quae explicabo, suscipit alias voluptas tenetur sit aperiam, soluta dolor minus.</p>
+
             <p></p>
         </div>
         <div class="alinear-derecha">
